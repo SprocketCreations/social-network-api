a1e268b11eb01d9e7117acdf9940ec39f0a04242
Kailen James <sevenflags57@gmail.com>
2023-02-23 16:45:00 -0800

    Got basic crud in for the routes

--- controllers/api/thoughts.js
+++ controllers/api/thoughts.js
@@ -1,40 +1,125 @@
+const { isValidObjectId } = require("mongoose");
+const { Thought, User } = require("../../models");
+
 const thoughts = require("express").Router();
 
 //#region Thoughts
 
-thoughts.get("/", (req, res) => {
-	return res.send("hello");
+thoughts.get("/", async (req, res) => {
+	try {
+		const thoughts = await Thought.find();
+		if (thoughts) {
+			return res.json({ thoughts });
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-thoughts.get("/:id", (req, res) => {
-	return res.send("hello");
+thoughts.get("/:id", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.id)) {
+			const thought = await Thought.findById(req.params.id);
+			if (thought) {
+				return res.json({ thought });
+			}
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-thoughts.post("/", (req, res) => {
-	// Add the new thought's id to the owner's user table.
-	return res.send("hello");
+thoughts.post("/", async (req, res) => {
+	try {
+		if (isValidObjectId(req.body.userId)) {
+			const user = await User.findById(req.body.userId);
+			if (user) {
+				const thought = await Thought.create({
+					body: req.body.body,
+					username: user.username
+				});
+				if (thought) {
+					// I guessed at this part and it worked???
+					user.thoughts.push(thought._id);
+					//TODO:?? somehow await this operation?
+					user.save();
+					return res.status(201).json({ thought });
+				}
+			}
+		}
+		return res.sendStatus(400);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-thoughts.put("/:id", (req, res) => {
-	return res.send("hello");
+thoughts.put("/:id", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.id)) {
+			const thought = await Thought.findByIdAndUpdate(req.params.id, {
+				text: req.body.text
+			});
+			if (thought) {
+				return res.status(200).json({ rows: 1 });
+			}
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-thoughts.delete("/:id", (req, res) => {
-	return res.send("hello");
+thoughts.delete("/:id", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.id) &&
+			await Thought.findByIdAndDelete(req.params.id)) {
+
+			return res.json({ rows: 1 });
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
 //#endregion Thoughts
 
 //#region Reactions
 
-thoughts.post("/:thoughtId/reactions", (req, res) => {
+thoughts.post("/:thoughtId/reactions", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.thoughtId)) {
+
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 	//Create a reaction and add it to the thought's table
-	return res.send("hello");
 });
 
-thoughts.delete("/:thoughtId/reactions/:reactionId", (req, res) => {
+thoughts.delete("/:thoughtId/reactions/:reactionId", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.thoughtId) &&
+			await Thought.findById(req.params.thoughtId) &&
+			isValidObjectId(req.params.reactionId)) {
+
+			
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 	// Remove a given reaction from the thought's table by id
-	return res.send("hello");
 });
 
 //#endregion Reactions
--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,29 +1,88 @@
 const users = require("express").Router();
-
+const { isValidObjectId } = require("mongoose");
+const { setTheUsername } = require("whatwg-url");
+const { User } = require("../../models");
 //#region Users
 
-users.get("/", (req, res) => {
-	// Return all users
-	return res.send("hello");
+users.get("/", async (req, res) => {
+	try {
+		// Return all users
+		const users = await User.find();
+		if (users) {
+			return res.json({ users });
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-users.get("/:id", (req, res) => {
-	// Return a user by their id
-	// include thoughts and friends
-	return res.send("hello");
+users.get("/:id", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.id)) {
+			const user = await User.findById(req.params.id);
+			// TODO: Return a user by their id
+			// include thoughts and friends
+			if (user) {
+				return res.json({ user });
+			}
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-users.post("/", (req, res) => {
-	return res.send("hello");
+users.post("/", async (req, res) => {
+	try {
+		const user = await User.create({
+			username: req.body.username,
+			email: req.body.email,
+		});
+		if (user) {
+			return res.status(201).json({ user });
+		}
+		return res.sendStatus(400);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-users.put("/:id", (req, res) => {
-	return res.send("hello");
+users.put("/:id", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.id)) {
+			const user = await User.findByIdAndUpdate(
+				req.params.id, {
+				username: req.body.username,
+				email: req.body.email,
+			});
+			if (user) {
+				return res.status(201).json({ rows: 1 });
+			}
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-users.delete("/:id", (req, res) => {
-	// remove user's thoughts as well
-	return res.send("hello");
+users.delete("/:id", async (req, res) => {
+	try {
+		if (isValidObjectId(req.params.id)) {
+			const user = await User.findByIdAndDelete(req.params.id).exec();
+			if (user) {
+				return res.json({ rows: 1 });
+			}
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
 //#endregion Users
@@ -31,12 +90,38 @@ users.delete("/:id", (req, res) => {
 
 users.post("/:userId/friends/:friendId", (req, res) => {
 	// add friendId to userId's friends list
-	return res.send("hello");
+	try {
+		if (isValidObjectId(req.params.userId) && isValidObjectId(req.params.friendId)) {
+
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
-users.delete("/:userId/friends/:friendId", (req, res) => {
+users.delete("/:userId/friends/:friendId", async (req, res) => {
 	// remove friendId to userId's friends list
-	return res.send("hello");
+	try {
+		if (isValidObjectId(req.params.friendId) &&
+			await User.findById(req.params.friendId) &&
+			isValidObjectId(req.params.userId)) {
+
+			const user =
+				await User.findByIdAndUpdate(
+					req.params.userId,
+					//TODO:
+					{
+						//friends: ????
+					}
+				);
+		}
+		return res.sendStatus(404);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
 });
 
 //#endregion Friends
